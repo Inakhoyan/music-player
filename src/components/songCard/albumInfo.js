a4294270ec24b5889b294934b5e387a8e5ff9b5a
Ishkhan <nakhoyan98@mail.ru>
2024-01-23 16:29:44 +0400

    player

---
+++ src/components/songCard/albumInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./albumInfo.css";
+
+export default function AlbumInfo({ album }) {
+	// console.log(album);
+  return (
+	<div>
+		<div className="albumName-container">
+
+		</div>
+		<div className="album-info">
+
+		</div>
+		<div className="album-release">
+
+		</div>
+	</div>
+  )
+}
---
+++ src/screens/player.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import "./player.css";
+import { useLocation } from 'react-router-dom';
+import apiClient from '../spotify';
+import Queue from '../components/queue';
+import SongCard from '../components/songCard';
+
+export default function Player() {
+
+	const location = useLocation();
+	const [tracks, setTracks] = useState([]);
+	const [currentTrack, setCurrentTrack] = useState({});
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	useEffect(() => {
+		if(location.state) {
+			apiClient
+			.get("playlists/" + location.state?.id + "/tarcks")
+			.then((res) => {
+				setTracks(res.data.items);
+				setCurrentTrack(res.data.items[0].track);
+			});
+		}
+	}, [location.state]);
+
+  return (
+	<div className='screen-container flex'>
+		<div className="left-player-body"></div>
+		<div className="right-player-body">
+			<SongCard album={currentTrack.album}/>
+			<Queue />
+		</div>
+	</div>
+  )
+}
